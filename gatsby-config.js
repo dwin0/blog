644f3ef9645e66c5f0865d84ba00fb4cced6d4be
David <david.windler@windowslive.com>
2020-04-22 22:20:14 +0200

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,8 +63,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-94082528-2`,
       },
     },
     {
