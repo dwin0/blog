0cfc6a0a2be0b473d9060a21b24e50390daa5542
David <david.windler@windowslive.com>
2020-04-30 01:33:03 +0200

    form v3

--- src/components/contact-form/index.js
+++ src/components/contact-form/index.js
@@ -1,37 +1,129 @@
-import React from 'react'
+import React, { useState, useReducer } from 'react'
 import { useIntl } from 'gatsby-plugin-intl'
 
-import { Form, FieldWrapper, Input, TextArea, SubmitButton } from './styles'
+import {
+  Form,
+  FieldWrapper,
+  Input,
+  TextArea,
+  SubmitButton,
+  SubmitError,
+  ButtonWrapper,
+  SubmitOKButton,
+} from './styles'
+
+const encode = (data) =>
+  Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+
+const initialState = {
+  botField: '',
+  name: '',
+  email: '',
+  message: '',
+}
+
+const formReducer = (state, { field, value }) => ({
+  ...state,
+  [field]: value,
+})
 
 const ContactForm = () => {
   const { formatMessage } = useIntl()
+  const [formState, dispatch] = useReducer(formReducer, initialState)
+  const { botField, name, email, message } = formState
+  const [submitError, setSubmitError] = useState('')
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
+  const handleChange = ({ target: { name, value } }) => {
+    dispatch({ field: name, value })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const form = event.target
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...formState,
+      }),
+    })
+      .then(() => {
+        setIsSubmitted(true)
+      })
+      .catch((error) => {
+        setSubmitError(error)
+      })
+  }
 
   return (
     <Form
       name="contact"
       method="POST"
-      action="/"
+      onSubmit={handleSubmit}
       data-netlify="true"
       data-netlify-honeypot="bot-field"
-      data-netlify-recaptcha="true"
     >
-      <input type="hidden" name="bot-field" />
+      <input
+        type="hidden"
+        name="bot-field"
+        value={botField}
+        onChange={handleChange}
+      />
       <FieldWrapper>
         <label htmlFor="name">{formatMessage({ id: 'name' })}</label>
-        <Input type="text" name="name" id="name" required />
+        <Input
+          type="text"
+          name="name"
+          id="name"
+          required
+          value={name}
+          onChange={handleChange}
+          disabled={isSubmitted}
+        />
       </FieldWrapper>
       <FieldWrapper>
         <label htmlFor="email">{formatMessage({ id: 'email' })}</label>
-        <Input type="email" name="email" id="email" required />
+        <Input
+          type="email"
+          name="email"
+          id="email"
+          required
+          value={email}
+          onChange={handleChange}
+          disabled={isSubmitted}
+        />
       </FieldWrapper>
       <FieldWrapper>
         <label htmlFor="message">{formatMessage({ id: 'message' })}</label>
-        <TextArea name="message" id="message" rows="4" required />
+        <TextArea
+          name="message"
+          id="message"
+          rows="4"
+          required
+          value={message}
+          onChange={handleChange}
+          disabled={isSubmitted}
+        />
       </FieldWrapper>
-      <div data-netlify-recaptcha="true"></div>
-      <SubmitButton type="submit">
-        {formatMessage({ id: 'sendForm' })}
-      </SubmitButton>
+      {submitError && <SubmitError>{submitError}</SubmitError>}
+      <ButtonWrapper>
+        <SubmitOKButton type="button" isSubmitted={isSubmitted}>
+          OK
+        </SubmitOKButton>
+        <SubmitButton
+          type="submit"
+          disabled={isSubmitted}
+          isSubmitted={isSubmitted}
+        >
+          {formatMessage({ id: 'sendForm' })}
+        </SubmitButton>
+      </ButtonWrapper>
     </Form>
   )
 }
--- src/components/contact-form/styles.js
+++ src/components/contact-form/styles.js
@@ -1,5 +1,11 @@
 import styled, { css } from 'styled-components'
-import { LIGHT_BLUE, WHITE } from '../../constants/colors'
+import {
+  LIGHT_BLUE,
+  WHITE,
+  RED,
+  DARK_GREEN,
+  GREY,
+} from '../../constants/colors'
 import { SPACE_1 } from '../../constants/space'
 
 const commonStyles = css`
@@ -21,23 +27,82 @@ export const FieldWrapper = styled.div`
   width: 100%;
 `
 
+const textInputStyles = css`
+  ${({ disabled }) =>
+    disabled
+      ? css`
+          background: ${GREY};
+        `
+      : css`
+          background: ${WHITE};
+        `};
+`
+
 export const Input = styled.input`
   ${commonStyles};
+  ${textInputStyles};
 `
 
 export const TextArea = styled.textarea`
   ${commonStyles};
+  ${textInputStyles};
 `
 
-export const SubmitButton = styled.button`
+export const ButtonWrapper = styled.div`
+  position: relative;
+  align-self: center;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+`
+
+const buttonStyle = css`
   ${commonStyles};
-  background: ${LIGHT_BLUE};
+  position: absolute;
   color: ${WHITE};
   margin: 0;
   border: 0;
   padding: 10px 20px;
+  width: 200px;
+  transition: transform 0.5s;
 
   &:active {
     transform: translate(2px, 2px);
   }
+`
+
+export const SubmitButton = styled.button`
+  ${buttonStyle};
+  background: ${LIGHT_BLUE};
+  transform-origin: top;
+
+  ${({ isSubmitted }) =>
+    isSubmitted
+      ? css`
+          transform: translateY(100%) rotateX(-90deg);
+        `
+      : css`
+          transform: translateY(0) rotateX(0);
+        `};
+`
+
+export const SubmitOKButton = styled.button`
+  ${buttonStyle};
+  background: ${DARK_GREEN};
+  transform-origin: bottom;
+
+  ${({ isSubmitted }) =>
+    isSubmitted
+      ? css`
+          transform: transform: translateY(0) rotateX(0);
+        `
+      : css`
+          transform: translateY(-100%) rotateX(90deg);
+        `};
+`
+
+export const SubmitError = styled.p`
+  color: ${RED};
+  font-weight: bold;
 `
