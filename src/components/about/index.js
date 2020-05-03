03c9a808be30161047260b3b430fbc0dffb33f60
David <david.windler@windowslive.com>
2020-05-03 22:46:54 +0200

    add twitter account on homepage

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useIntl } from 'gatsby-plugin-intl'
 import { ic_mood } from 'react-icons-kit/md/ic_mood'
+import { TwitterIcon } from 'react-share'
 
 import { Wrapper, Icon, Text } from './styles'
 
@@ -14,7 +15,24 @@ export const About = () => {
       onMouseLeave={() => setHovered(false)}
     >
       <Icon size={64} icon={ic_mood} rotate={hovered} />
-      <Text>{intl.formatMessage({ id: 'aboutMe' })}</Text>
+      <Text>
+        <span>{intl.formatMessage({ id: 'aboutMe' })}</span>
+        <br />
+        <br />
+        <span>{intl.formatMessage({ id: 'followOnTwitter' })}</span>
+        <span>
+          <a
+            href="https://twitter.com/WebDeveloperBl1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            &nbsp;
+            <TwitterIcon size={20} round />
+            &nbsp; Twitter
+          </a>
+          .
+        </span>
+      </Text>
     </Wrapper>
   )
 }
